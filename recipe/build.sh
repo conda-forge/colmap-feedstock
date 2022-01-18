e847ad756a81fa93e9cbc99fd1635a662ef1048d
Tobias Fischer <info@tobiasfischer.info>
2022-01-18 10:57:48 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,19 @@
 mkdir build
 cd build
 
+
+if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" ]]
+then
+    CUDA_ENABLED=ON
+else
+    CUDA_ENABLED=OFF
+fi
+
+
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DBOOST_STATIC=OFF \
-      -DCUDA_ENABLED=OFF \
+      -DCUDA_ENABLED=${CUDA_ENABLED} \
       -DBUILD_SHARED_LIBS=ON \
       ..
 
