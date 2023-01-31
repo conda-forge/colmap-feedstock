99d4ca2b7af4925ef7e83ce210349cea626d1e3c
Tobias Fischer <info@tobiasfischer.info>
2023-02-01 07:08:48 +1000

    Try static libs for now

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,8 +13,7 @@ if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" 
 cmake ${CMAKE_ARGS} \
       -DCMAKE_BUILD_TYPE=Release \
       -DBOOST_STATIC=OFF \
-      -DBUILD_SHARED_LIBS=ON \
-      -DCMAKE_INSTALL_LIBDIR=lib \
+      -DBUILD_SHARED_LIBS=OFF \
       ${EXTRA_CMAKE_ARGS} \
 ..
 
